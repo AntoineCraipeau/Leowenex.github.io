1712cc6051036231d9f97ae3ef03360a1d24d3a8
Kunthearo <nothyroo@gmail.com>
2022-05-19 09:27:44 +0200

    change

--- script.js
+++ script.js
@@ -1,63 +1,323 @@
-const data = [
+var data = [
     {
         "title":"GUILTY GEAR -STRIVE-",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
         "studio":"Arc System Works",
         "editor":"Bandai Namco",
         "release":"2021",
+        "cat":"vsfighter"
     }, 
     {
         "title":"Street Fighter V",
+        "url":"https://s1.gaming-cdn.com/images/products/5820/orig-fallback-v1/street-fighter-v-champion-edition-upgrade-kit-bundle-pc-jeu-steam-cover.jpg",
         "studio":"Capcom",
         "editor":"Capcom",
-        "release":"2016"
+        "release":"2016",
+        "cat":"vsfighter"
     }, 
     {
         "title":"Blazblue: Cross Tag Battle",
+        "url":"https://image.jeuxvideo.com/medias/152085/1520848161-5549-jaquette-avant.jpg",
         "studio":"Arc System Works",
         "editor":"Arc System Works",
-        "release":"2018"
+        "release":"2018",
+        "cat":"vsfighter"
     },
     {
         "title":"Dragon Ball FighterZ",
+        "url":"https://cdn-prod.scalefast.com/public/assets/user/122595/image/6d380937c8407749e846865c8a40f58b.jpg",
         "studio":"Arc System Works",
         "editor":"Bandai Namco Entertainment",
-        "release":"2018"
+        "release":"2018",
+        "cat":"vsfighter"
     },
     {
         "title":"SoulCalibur VI",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
         "studio":"Project Soul",
         "editor":"Bandai Namco Entertainment",
-        "release":"2018"
+        "release":"2018",
+        "cat":"vsfighter"
     },
     {
         "title":"Tekken 7",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
         "studio":"Bandai Namco Studios",
         "editor":"Bandai Namco Entertainment",
-        "release":"2015"
+        "release":"2015",
+        "cat":"vsfighter"
     },
     {
         "title":"One Piece: Burning Blood",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
         "studio":"Spike Chunsoft",
         "editor":"Bandai Namco Entertainment",
-        "release":"2016"
+        "release":"2016",
+        "cat":"vsfighter"
     },
     {
         "title":"Super Smash Bros. Ultimate",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
         "studio":"Sora",
         "editor":"Nintendo",
-        "release":"2018"
+        "release":"2018",
+        "cat":"vsfighter"
     },
     {
         "title":"Naruto Shippuden: Ultimate Ninja Storm 4",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
         "studio":"CyberConnect2",
         "editor":"Bandai Namco Entertainment",
-        "release":"2017"
+        "release":"2017",
+        "cat":"vsfighter"
     },
     {
         "title":"Mortal Kombat 11",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
         "studio":"NetherRealm Studios",
         "editor":"Warner Bros. Interactive Entertainment",
-        "release":"2019"
+        "release":"2019",
+        "cat":"vsfighter"
+    },
+    {
+        "title":"Sims 4",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Maxis",
+        "editor":"EA",
+        "release":"2014",
+        "cat":"simulator"
+    }, 
+    {
+        "title":"Euro Truck Simulator 2",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"SCS Software",
+        "editor":"SCS Software",
+        "release":"2012",
+        "cat":"simulator"
+    }, 
+    {
+        "title":"Bus Simulator 21",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Stillalive Studios",
+        "editor":"Astragon",
+        "release":"2021",
+        "cat":"simulator"
+    },
+    {
+        "title":"Animal Crossing: New Horizons",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Nintendo EPD",
+        "editor":"Nintendo",
+        "release":"2020",
+        "cat":"simulator"
+    },
+    {
+        "title":"Goat Simulator",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Coffee Stain Studios",
+        "editor":"Cofee Stain Studios",
+        "release":"2014",
+        "cat":"simulator"
+    },
+    {
+        "title":"Youtubers Life 2",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"UPlay Online",
+        "editor":"Raiser Games",
+        "release":"2021",
+        "cat":"simulator"
+    },
+    {
+        "title":"Flight Simulator",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Asobo Studio",
+        "editor":"Aerosoft",
+        "release":"2020",
+        "cat":"simulator"
+    },
+    {
+        "title":"Farming Simulator",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"GIANTS Software",
+        "editor":"GIANTS Software",
+        "release":"2019",
+        "cat":"simulator"
+    },
+    {
+        "title":"Hunting Simulator 2",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Neopica",
+        "editor":"Nacon",
+        "release":"2020",
+        "cat":"simulator"
+    },
+    {
+        "title":"Ultimate Fishing Simulator 2",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"MasterCode",
+        "editor":"Ultimate Games",
+        "release":"2021",
+        "cat":"simulator"
+    },
+    {
+        "title":"FIFA 22",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"EA Vancouver",
+        "editor":"EA",
+        "release":"2021",
+        "cat":"sport"
+    }, 
+    {
+        "title":"NBA 2K22 2k",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"2K Games",
+        "editor":"2K Games",
+        "release":"2021",
+        "cat":"sport"
+    }, 
+    {
+        "title":"Nintendo Switch Sports",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Nintendo",
+        "editor":"Nintendo",
+        "release":"2022",
+        "cat":"sport"
+    },
+    {
+        "title":"Riders Republic",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Ubisoft",
+        "editor":"Ubisoft",
+        "release":"2021",
+        "cat":"sport"
+    },
+    {
+        "title":"MLB: The Show 22",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"SIE San Diego Studio",
+        "editor":"Sony interactive Entertainment",
+        "release":"2022",
+        "cat":"sport"
+    },
+    {
+        "title":"Ring Fit Adventure",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Nintendo Entertainment Planning & Development",
+        "editor":"Nintendo",
+        "release":"2019",
+        "cat":"sport"
+    },
+    {
+        "title":"Madden NFL 22",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"EA",
+        "editor":"EA",
+        "release":"2020",
+        "cat":"sport"
+    },
+    {
+        "title":"NHL",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"EA Vancouver",
+        "editor":"EA",
+        "release":"2021",
+        "cat":"sport"
+    },
+    {
+        "title":"Big Rumble Boxing: Creed Champions",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Survios",
+        "editor":"Survios",
+        "release":"2021",
+        "cat":"sport"
+    },
+    {
+        "title":"Mario Golf: Super Rush",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Camelot Software Planning",
+        "editor":"Nintendo",
+        "release":"2021",
+        "cat":"sport"
+    },
+    {
+        "title":"Age of Empires IV",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Relic Entertainment",
+        "editor":"Xbox Games Studios",
+        "release":"2021",
+        "cat":"strategy"
+    }, 
+    {
+        "title":"Humankind",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Amplitude Studios",
+        "editor":"Sega",
+        "release":"2020",
+        "cat":"strategy"
+    }, 
+    {
+        "title":"Civilization VI",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Firaxis Games",
+        "editor":"2K Games",
+        "release":"2016",
+        "cat":"strategy"
+    },
+    {
+        "title":"Desperados III",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Mimimi Games",
+        "editor":"THQ Nordic",
+        "release":"2020",
+        "cat":"strategy"
+    },
+    {
+        "title":"Fire Emblem: Awakening",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Nintendo",
+        "editor":"Nintendo",
+        "release":"2012",
+        "cat":"strategy"
+    },
+    {
+        "title":"Stellaris",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Paradox Development Studio",
+        "editor":"Paradox Interactive",
+        "release":"2016",
+        "cat":"strategy"
+    },
+    {
+        "title":"Northgard",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Shiro Games",
+        "editor":"Shiro Games",
+        "release":"2017",
+        "cat":"strategy"
+    },
+    {
+        "title":"XCOM 2",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Feral Interactive",
+        "editor":"2K Games",
+        "release":"2016",
+        "cat":"strategy"
+    },
+    {
+        "title":"Crusader Kings III",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Paradox Developement Studio",
+        "editor":"Paradox Interactive",
+        "release":"2020",
+        "cat":"strategy"
+    },
+    {
+        "title":"Total War: Warhammer 2",
+        "url":"https://s1.gaming-cdn.com/images/products/6948/orig-fallback-v1/jeu-steam-guilty-gear-strive-cover.jpg",
+        "studio":"Creative Assembly",
+        "editor":"Feral Interactive",
+        "release":"2017",
+        "cat":"strategy"
     }
 ]
 
@@ -69,6 +329,7 @@ function show(data){
     let tab = 
         `<tr>
             <th>Title</th>
+            <th>Image</th>
             <th>Studio</th>
             <th>Editor</th>
             <th>Release</th>
@@ -77,6 +338,7 @@ function show(data){
     for (let r of data){
         tab += `<tr>
         <td>${r.title}</td>
+        <td><img src="${r.url}" width="300" height="400"></td>
         <td>${r.studio}</td>
         <td>${r.editor}</td>
         <td>${r.release}</td>
@@ -84,4 +346,8 @@ function show(data){
     }
 
     document.getElementById("tables").innerHTML = tab;
+}
+
+function tdclick(r){
+    console.log("hello");
 }

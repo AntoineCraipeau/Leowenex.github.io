901c2109297a64f796e8f10aa9d87a3d59ace093
Leowenex <antoinecraipeau@outlook.fr>
2022-05-19 12:00:52 +0200

    push

--- addgame.js
+++ addgame.js
@@ -20,25 +20,37 @@ function add(game){
 
 	var tr = document.createElement("tr");
 
+    var image = document.createElement("img");
+    image.src = game.url;
+    image.width = "300";
+    image.height = "400";
+    tr.append(image);
+
+    var infodiv = document.createElement("div");
+    infodiv.className = "gameinfo";
+
+
 	var tdtitle = document.createElement("td");
 	tdtitle.appendChild(document.createTextNode(game.title));
-    tr.appendChild(tdtitle);
+    infodiv.appendChild(tdtitle);
 
     var tdcat = document.createElement("td");
 	tdcat.appendChild(document.createTextNode(game.cat));
-    tr.appendChild(tdcat);
+    infodiv.appendChild(tdcat);
 
 	var tdstudio = document.createElement("td");
 	tdstudio.appendChild(document.createTextNode(game.studio));
-    tr.appendChild(tdstudio);
+    infodiv.appendChild(tdstudio);
 
 	var tdpublisher = document.createElement("td");
 	tdpublisher.appendChild(document.createTextNode(game.editor));
-    tr.appendChild(tdpublisher);
+    infodiv.appendChild(tdpublisher);
 
     var tdrelease = document.createElement("td");
 	tdrelease.appendChild(document.createTextNode(game.release));
-    tr.appendChild(tdrelease);
+    infodiv.appendChild(tdrelease);
+
+    tr.appendChild(infodiv);
 
 	var currentDiv = document.getElementById('game-list');
 	console.log(currentDiv);
@@ -52,6 +64,7 @@ function del(){
 	for (var i = 1; i < length; i++) {
     	table.deleteRow(1);
 	}
+    list = [];
 }
 
 function save(){
